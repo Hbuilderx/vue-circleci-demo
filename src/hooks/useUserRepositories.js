64aea0bea91006adff9acde9d3e2daeb0cfa606e
Hbuilderx <285085215@qq.com>
2022-04-09 18:54:30 +0800

    接口异常处理

--- src/hooks/useUserRepositories.js
+++ src/hooks/useUserRepositories.js
@@ -3,7 +3,25 @@ import { ref, onMounted, watch } from 'vue'
 export default function useUserRepositories(user) {
   const repositories = ref([])
   const getUserRepositories = async () => {
-    repositories.value = await fetchUserRepositories(user.value)
+    try{
+      repositories.value = await fetchUserRepositories(user.value)
+    }catch(err){
+      if(err){
+        repositories.value = [
+          {
+            name: "zhu",
+            username: "dongshuai",
+            email: "ratszhu@gmail.com",
+            phone: "18855055159",    
+            address: {
+              city: "SuZhou",
+              street: "NanShi Street",
+              suite: "BaiTang"
+            }
+          }
+        ]
+      }
+    }
   }
   onMounted(getUserRepositories)
   watch(user, getUserRepositories)
